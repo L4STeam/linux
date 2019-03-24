4247a384825648c429b64ebdce8bc414976dbd7c
David Lebrun <dlebrun@google.com>
2019-03-24 13:41:35 +0100

    tcp: limit the AccECN counter update diff to prevent wrap-around
    The AccECN counter being encoded on 3 bits, sending a diff higher than 7
    will prevent the receiver to detect wrap-around. This patch bounds the
    maximum diff to 6.
    
    Signed-off-by: David Lebrun <dlebrun@google.com>

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -286,6 +286,7 @@ struct tcp_sock {
 	u32	delivered;	/* Total data packets delivered incl. rexmits */
 	u32	delivered_ce;	/* Like the above but only ECE marked packets */
 	u32	received_ce;	/* Like the above but for received CE marked packets */
+	u32	received_ce_tx; /* Like the above but max transmitted value */
 	u32	lost;		/* Total data packets lost incl. rexmits */
 	u32	app_limited;	/* limited until "delivered" reaches this val */
 	u64	first_tx_mstamp;  /* start of window send phase */
