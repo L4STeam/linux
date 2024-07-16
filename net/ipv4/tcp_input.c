89bf657915f951886f2d9be30da81b3cf32ee085
Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
2024-07-16 13:02:58 +0200

    tcp: accecn: unset ECT if receive or send ACE=0 in AccECN negotiaion
    Based on specification:
      https://tools.ietf.org/id/draft-ietf-tcpm-accurate-ecn-28.txt
    
    Based on Section 3.1.5 of AccECN spec (RFC9768), a TCP Server in
    AccECN mode MUST NOT set ECT on any packet for the rest of the connection,
    if it has received or sent at least one valid SYN or Acceptable SYN/ACK
    with (AE,CWR,ECE) = (0,0,0) during the handshake.
    
    In addition, a host in AccECN mode that is feeding back the IP-ECN
    field on a SYN or SYN/ACK MUST feed back the IP-ECN field on the
    latest valid SYN or acceptable SYN/ACK to arrive.
    
    Signed-off-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -6170,6 +6170,8 @@ static bool tcp_validate_incoming(struct sock *sk, struct sk_buff *skb,
 	if (th->syn) {
 		if (tcp_ecn_mode_accecn(tp)) {
 			accecn_reflector = true;
+			tp->syn_ect_rcv = TCP_SKB_CB(skb)->ip_dsfield &
+					  INET_ECN_MASK;
 			if (tp->rx_opt.accecn &&
 			    tp->saw_accecn_opt < TCP_ACCECN_OPT_COUNTER_SEEN) {
 				u8 saw_opt = tcp_accecn_option_init(skb, tp->rx_opt.accecn);
