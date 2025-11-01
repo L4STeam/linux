34654597143603836b9a29cf3d4c513e8245d33f
Ilpo Järvinen <ij@kernel.org>
2025-11-01 19:55:59 +0100

    tcp: accecn: AccECN negotiation
    Accurate ECN negotiation parts based on the specification:
      https://tools.ietf.org/id/draft-ietf-tcpm-accurate-ecn-28.txt
    
    Accurate ECN is negotiated using ECE, CWR and AE flags in the
    TCP header. TCP falls back into using RFC3168 ECN if one of the
    ends supports only RFC3168-style ECN.
    
    The AccECN negotiation includes reflecting IP ECN field value
    seen in SYN and SYNACK back using the same bits as negotiation
    to allow responding to SYN CE marks and to detect ECN field
    mangling. CE marks should not occur currently because SYN=1
    segments are sent with Non-ECT in IP ECN field (but proposal
    exists to remove this restriction).
    
    Reflecting SYN IP ECN field in SYNACK is relatively simple.
    Reflecting SYNACK IP ECN field in the final/third ACK of
    the handshake is more challenging. Linux TCP code is not well
    prepared for using the final/third ACK a signalling channel
    which makes things somewhat complicated here.
    
    tcp_ecn sysctl can be used to select the highest ECN variant
    (Accurate ECN, ECN, No ECN) that is attemped to be negotiated and
    requested for incoming connection and outgoing connection:
    TCP_ECN_IN_NOECN_OUT_NOECN, TCP_ECN_IN_ECN_OUT_ECN,
    TCP_ECN_IN_ECN_OUT_NOECN, TCP_ECN_IN_ACCECN_OUT_ACCECN,
    TCP_ECN_IN_ACCECN_OUT_ECN, and TCP_ECN_IN_ACCECN_OUT_NOECN.
    
    Signed-off-by: Ilpo Järvinen <ij@kernel.org>
    Co-developed-by: Olivier Tilmans <olivier.tilmans@nokia.com>
    Signed-off-by: Olivier Tilmans <olivier.tilmans@nokia.com>
    Co-developed-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
    Signed-off-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -45,6 +45,7 @@ static unsigned int tcp_child_ehash_entries_max = 16 * 1024 * 1024;
 static unsigned int udp_child_hash_entries_max = UDP_HTABLE_SIZE_MAX;
 static int tcp_plb_max_rounds = 31;
 static int tcp_plb_max_cong_thresh = 256;
+static int tcp_ecn_mode_max = 2;
 
 /* obsolete */
 static int sysctl_tcp_low_latency __read_mostly;
@@ -726,7 +727,7 @@ static struct ctl_table ipv4_net_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dou8vec_minmax,
 		.extra1		= SYSCTL_ZERO,
-		.extra2		= SYSCTL_TWO,
+		.extra2		= &tcp_ecn_mode_max,
 	},
 	{
 		.procname	= "tcp_ecn_fallback",
