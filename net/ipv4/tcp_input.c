29015d9e2323faad4351ca479f3c3cd2b970b141
Ilpo Järvinen <ij@kernel.org>
2025-11-01 21:11:58 +0100

    tcp: accecn: add AccECN rx byte counters
    These three byte counters track IP ECN field payload byte sums for
    all arriving (acceptable) packets for ECT0, ECT1, and CE. The
    AccECN option (added by a later patch in the series) echoes these
    counters back to sender side; therefore, it is placed within the
    group of tcp_sock_write_txrx.
    
    Signed-off-by: Ilpo Järvinen <ij@kernel.org>
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Co-developed-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
    Signed-off-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -6256,7 +6256,7 @@ void tcp_rcv_established(struct sock *sk, struct sk_buff *skb)
 					flag |= __tcp_replace_ts_recent(tp,
 									delta);
 
-				tcp_ecn_received_counters(sk, skb);
+				tcp_ecn_received_counters(sk, skb, 0);
 
 				/* We know that such packets are checksummed
 				 * on entry.
@@ -6302,7 +6302,8 @@ void tcp_rcv_established(struct sock *sk, struct sk_buff *skb)
 			/* Bulk data transfer: receiver */
 			tcp_cleanup_skb(skb);
 			__skb_pull(skb, tcp_header_len);
-			tcp_ecn_received_counters(sk, skb);
+			tcp_ecn_received_counters(sk, skb,
+						  len - tcp_header_len);
 			eaten = tcp_queue_rcv(sk, skb, &fragstolen);
 
 			tcp_event_data_recv(sk, skb);
@@ -6343,7 +6344,7 @@ slow_path:
 		return;
 
 step5:
-	tcp_ecn_received_counters(sk, skb);
+	tcp_ecn_received_counters_payload(sk, skb);
 
 	reason = tcp_ack(sk, skb, FLAG_SLOWPATH | FLAG_UPDATE_TS_RECENT);
 	if ((int)reason < 0) {
