d4c0dbb82c8b0daa54c69f61af88ed296377ac70
Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
2025-11-02 18:28:26 +0100

    tcp: disable RFC3168 fallback identifier for CC modules
    When AccECN is not successfully negociated for a TCP flow, it defaults
    fallback to classic ECN (RFC3168). However, L4S service will fallback
    to non-ECN.
    
    This patch enables congestion control module to control whether it
    should not fallback to classic ECN after unsuccessful AccECN negotiation.
    A new CA module flag (TCP_CONG_NO_FALLBACK_RFC3168) identifies this
    behavior expected by the CA.
    
    Signed-off-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1176,8 +1176,11 @@ enum tcp_ca_ack_event_flags {
 #define TCP_CONG_NEEDS_ACCECN		BIT(2)
 /* Use ECT(1) instead of ECT(0) while the CA is uninitialized */
 #define TCP_CONG_WANTS_ECT_1		BIT(3)
+/* Cannot fallback to RFC3168 during AccECN negotiation */
+#define TCP_CONG_NO_FALLBACK_RFC3168	BIT(4)
 #define TCP_CONG_MASK	(TCP_CONG_NON_RESTRICTED | TCP_CONG_NEEDS_ECN | \
-			TCP_CONG_NEEDS_ACCECN | TCP_CONG_WANTS_ECT_1)
+			TCP_CONG_NEEDS_ACCECN | TCP_CONG_WANTS_ECT_1 | \
+			TCP_CONG_NO_FALLBACK_RFC3168)
 
 union tcp_cc_info;
 
@@ -1314,6 +1317,13 @@ static inline bool tcp_ca_needs_accecn(const struct sock *sk)
 	const struct inet_connection_sock *icsk = inet_csk(sk);
 
 	return icsk->icsk_ca_ops->flags & TCP_CONG_NEEDS_ACCECN;
+}
+
+static inline bool tcp_ca_no_fallback_rfc3168(const struct sock *sk)
+{
+	const struct inet_connection_sock *icsk = inet_csk(sk);
+
+	return icsk->icsk_ca_ops->flags & TCP_CONG_NO_FALLBACK_RFC3168;
 }
 
 static inline bool tcp_ca_wants_ect_1(const struct sock *sk)
