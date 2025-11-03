7d28b196152d997e8273b869efdff800575f7f46
Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
2025-11-03 10:28:08 +0100

    tcp: Add mss_cache_set_by_cca for CC algorithm to set MSS
    Make the CC module set the mss_cache smaller than path mtu. This is useful
    for a CC module that maintains an internal fractional cwnd less than 2 at
    very low speed (<100kbps) and very low RTT (<1ms). In this case, the
    minimum snd_cwnd for the stack remains at 2, but the CC module will limit
    the pacing rate to ensure that its internal fractional cwnd takes effect.
    Therefore, the CC algorithm can enable fine-grained control without
    causing big rate saw-tooth and delay jitter.
    
    Signed-off-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -232,7 +232,8 @@ struct tcp_sock {
 		repair      : 1,
 		tcp_usec_ts : 1, /* TSval values in usec */
 		is_sack_reneg:1,    /* in recovery from loss with SACK reneg? */
-		is_cwnd_limited:1;/* forward progress limited by snd_cwnd? */
+		is_cwnd_limited:1,/* forward progress limited by snd_cwnd? */
+		mss_cache_set_by_cca:1;/* mss_cache set by CCA */
 	__cacheline_group_end(tcp_sock_read_txrx);
 
 	/* RX read-mostly hotpath cache lines */
--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2002,7 +2002,7 @@ unsigned int tcp_sync_mss(struct sock *sk, u32 pmtu)
 	struct inet_connection_sock *icsk = inet_csk(sk);
 	int mss_now;
 
-	if (icsk->icsk_mtup.search_high > pmtu)
+	if (icsk->icsk_mtup.search_high > pmtu && !tp->mss_cache_set_by_cca)
 		icsk->icsk_mtup.search_high = pmtu;
 
 	mss_now = tcp_mtu_to_mss(sk, pmtu);
@@ -2032,7 +2032,7 @@ unsigned int tcp_current_mss(struct sock *sk)
 
 	mss_now = tp->mss_cache;
 
-	if (dst) {
+	if (dst && !tp->mss_cache_set_by_cca) {
 		u32 mtu = dst_mtu(dst);
 		if (mtu != inet_csk(sk)->icsk_pmtu_cookie)
 			mss_now = tcp_sync_mss(sk, mtu);
